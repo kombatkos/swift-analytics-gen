9c3bbd173239b7342ffd8ff9b6171eeadc2b5b12
Konstantin Porokhov <konstantinporokhov@MacBook-Pro-Konstantin.local>
2024-10-21 19:41:15 +0400

    changed min ios version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "AnalyticsGen",
     platforms: [
         .macOS(.v10_12),
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .executable(name: "AnalyticsGen", targets: ["AnalyticsGen"]),
